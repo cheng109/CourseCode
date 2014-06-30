acfc4b675e91f97e62d1e216da46fa1035980059
Jun Cheng <cheng109@hansen-fe00.rcac.purdue.edu>
2014-06-30 14:34:38 -0400

    Update for HW03

---
+++ Summer2014/PA03/echo-lite.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+  int i; 
+  if(argc==1)
+    return EXIT_SUCCESS;
+  else {
+    for(i=1; i<argc; i++) {
+      printf("%s ", argv[i]); 
+    }
+    printf("\n");
+    return EXIT_SUCCESS; 
+  }
+}
