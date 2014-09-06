2a5381c46334956890e6a79988a8b8c3f20ef262
Jun Cheng <juncheng@Juns-MacBook-Pro-2.local>
2014-09-06 18:48:28 -0400

    hw4 done

--- ece308_Fall2014/HW4/Base.java
+++ ece308_Fall2014/HW4/Base.java
@@ -1,6 +1,21 @@
 public class Base {
 
-   public Base( ) { }
+   public Base( ) {
+  	   
+   }
+	   
+   void f1() {
+	   System.out.println("Base f1()"); 
+   }
+   	
+   void f1(int t) {
+	   System.out.println("Base f1(int)"); 
+   }
+	   
+	   
+	   
+	   
+
 
   // Add any needed method(s) here.
 
--- ece308_Fall2014/HW4/Derived.java
+++ ece308_Fall2014/HW4/Derived.java
@@ -1,7 +1,15 @@
 public class Derived extends Base {
 
    public Derived( ) { }
-
+   
+   
+   void f1(float t) {
+	   System.out.println("Derived f1(float)"); 
+   }
+	  
+   void f1() {
+	   System.out.println("Base f1"); 
+   }
   // Add any needed function(s) here.
 
 }
