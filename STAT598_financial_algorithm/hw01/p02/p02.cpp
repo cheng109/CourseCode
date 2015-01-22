5bc0de904c4227448712bb1275c45e6c323a81fe
Jun Cheng <cheng109@purdue.edu>
2015-01-22 10:28:31 -0500

    done p02

--- STAT598_financial_algorithm/hw01/p02/p02.cpp
+++ STAT598_financial_algorithm/hw01/p02/p02.cpp
@@ -5,13 +5,17 @@
 using namespace std;
 
 int combine(int n, int k) {
-	if(n==k || k==0) return 1;
-	return combine(n-1,k-1)+combine(n-1, k);
+  if(n==k || k==0) {
+      cout << " C(" << n << ", " << k << ")" << endl ;
+      return 1;
+  
+  }
+  return combine(n-1,k-1)+combine(n-1, k);
 }
 
 int main() {
-	cout << combine(30, 6) << endl;
-	return SUCCESS;
+  cout << combine(5, 3) << endl;
+  return SUCCESS;
 }
 
 
