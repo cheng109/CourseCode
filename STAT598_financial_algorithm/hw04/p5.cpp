38fc283acfdb99081d3dbc5e410592d980d25824
cheng109 <cheng109@extrad.physics.purdue.edu>
2015-04-09 13:50:51 -0400

    problem 5 done

--- STAT598_financial_algorithm/hw04/p5.cpp
+++ STAT598_financial_algorithm/hw04/p5.cpp
@@ -2,14 +2,99 @@
 #include <vector>
 #include <fstream> 
 #include <string> 
+#include <limits.h>
+#include <queue> 
+
 using namespace std; 
 
 enum Direction{EAST, WEST, SOUTH, NORTH, UP, DOWN}; 
-typedef vector<vector<vector<char> > > array3D; 
 
-void solveMaze(array3D maze) {
+typedef vector<vector<vector<char> > > array3D;
+
+template<typename T>
+vector<vector<vector<T> > > create3Darray(T val){
+  vector<vector<vector<T> > >  maze; 
+  int h=3; 
+  int m=3; 
+  int n=3; 
+  maze.resize(h); 
+  for(int i=0; i<h; ++i) {
+    maze[i].resize(m); 
+    for(int j=0; j<m; ++j) 
+      maze[i][j].resize(n,val); 
+  }
+  return maze; 
+}
+
+struct index {
+  int i, j, k; 
+  index(int a, int b, int c): i(a), j(b),k(c) {}
+}; 
   
+void solveMaze(array3D maze) {
+  vector<vector<vector<int> > >  dist = create3Darray(INT_MAX);
+  vector<Direction> vD; 
+  vector<vector<vector<vector<Direction> > > > Path = create3Darray(vD) ;  
+  queue<index::index> q;
+  index::index V(0,0,0);
+  q.push(V); 
+  dist[0][0][0] = 0; 
+  while(!q.empty()) {
+    index::index c = q.front(); 
+    //Down
+    if(c.i+1<3 && maze[c.i+1][c.j][c.k]!='o' && dist[c.i+1][c.j][c.k]==INT_MAX) {
+      q.push(index::index(c.i+1, c.j, c.k)); 
+      dist[c.i+1][c.j][c.k] = dist[c.i][c.j][c.k] + 1;
+      Path[c.i+1][c.j][c.k] = Path[c.i][c.j][c.k]; 
+      Path[c.i+1][c.j][c.k].push_back(DOWN); 
+    }
+    //up
+    if(c.i-1>=0 && maze[c.i-1][c.j][c.k]!='o' && dist[c.i-1][c.j][c.k]==INT_MAX) {
+      q.push(index::index(c.i-1, c.j, c.k)); 
+      dist[c.i-1][c.j][c.k] = dist[c.i][c.j][c.k] + 1; 
+      Path[c.i-1][c.j][c.k] = Path[c.i][c.j][c.k]; 
+      Path[c.i-1][c.j][c.k].push_back(UP); 
 
+    }
+    // South
+    if(c.j+1<3 && maze[c.i][c.j+1][c.k]!='o' && dist[c.i][c.j+1][c.k]==INT_MAX) {
+      q.push(index::index(c.i, c.j+1, c.k)); 
+      dist[c.i][c.j+1][c.k] = dist[c.i][c.j][c.k] + 1; 
+      Path[c.i][c.j+1][c.k] = Path[c.i][c.j][c.k]; 
+      Path[c.i][c.j+1][c.k].push_back(SOUTH); 
+
+    }
+    // North
+    if(c.j-1>=0 && maze[c.i][c.j-1][c.k]!='o' && dist[c.i][c.j-1][c.k]==INT_MAX) {
+      q.push(index::index(c.i, c.j-1, c.k)); 
+      dist[c.i][c.j-1][c.k] = dist[c.i][c.j][c.k] + 1; 
+      Path[c.i][c.j-1][c.k] = Path[c.i][c.j][c.k]; 
+      Path[c.i][c.j-1][c.k].push_back(NORTH); 
+
+    }
+    // East
+    if(c.k+1<3 && maze[c.i][c.j][c.k+1]!='o' && dist[c.i][c.j][c.k+1]==INT_MAX) {
+      q.push(index::index(c.i, c.j, c.k+1)); 
+      dist[c.i][c.j][c.k+1] = dist[c.i][c.j][c.k] + 1; 
+      Path[c.i][c.j][c.k+1] = Path[c.i][c.j][c.k]; 
+      Path[c.i][c.j][c.k+1].push_back(EAST); 
+
+    }
+    // West
+    if(c.k-1>=0 && maze[c.i][c.j][c.k-1]!='o' && dist[c.i][c.j][c.k-1]==INT_MAX) {
+      q.push(index::index(c.i, c.j, c.k-1)); 
+      dist[c.i][c.j][c.k-1] = dist[c.i][c.j][c.k] + 1; 
+      Path[c.i][c.j][c.k-1] = Path[c.i][c.j][c.k]; 
+      Path[c.i][c.j][c.k-1].push_back(WEST); 
+
+    }
+
+    q.pop(); 
+  }
+  
+  for(int i=0; i<Path[2][2][2].size(); ++i) {
+    cout << Path[2][2][2][i] << endl;
+  }
 } 
 
 array3D readmaze(string fileName) {
@@ -36,31 +121,28 @@ array3D readmaze(string fileName) {
   return v; 
 }
 
- 
-int main() {
-  array3D maze; 
-  int h=3; 
-  int m=3; 
-  int n=3; 
-  maze.resize(h); 
-  for(int i=0; i<h; ++i) {
-    maze[i].resize(m); 
-    for(int j=0; j<m; ++j) 
-      maze[i][j].resize(n); 
-  }
+
   
+
+int main() {
+  array3D maze = create3Darray('0'); 
   
   maze = readmaze("maze.txt"); 
-  for(int i=0; i<maze.size();++i) {
-    for(int j=0; j<maze[i].size() ; ++j) {
-      for(int k=0; k<maze[i][j].size(); ++k) {
-	cout << maze[i][j][k]; 
+
+  for(int i=0; i<3; ++i) {
+    for(int j=0; j<3; ++j) {
+      for(int k=0; k<3; ++k) {
+	cout << maze[i][j][k] << "\t" ; 
       }
-      cout<<endl; 
+      cout<< endl; 
     }
     cout << endl; 
   }
-  cout << maze.size() << endl; 
+  
+
+
+
+
   solveMaze(maze); 
   return 0; 
 }
