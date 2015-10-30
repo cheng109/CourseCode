469a1ab1e29dcc17ea7e35a0a93c15a52407774a
Jun Cheng <cheng109@purdue.edu>
2015-10-30 19:24:20 -0400

    add

--- CS515_Matrix/hw06/jacobian.m
+++ CS515_Matrix/hw06/jacobian.m
@@ -1,23 +1,30 @@
 function [U, iter] = jacobian(A, b)
+
+% Assume A is a sparse matrix
 [N,t] = size(b); 
-U = zeros(N,1) ; 
+U = zeros(N,1) ;
 %diff = zeros(20, 1) ; 
-true = A\b; 
-diff = 100; 
+[J,I,V] = find(A); 
+res = 100; 
 iter = 0 ; 
-while diff > 1.0e-4 
+nb = norm(b); 
+[len,t]= size(I);  
+
+while res > 1.0e-4 
+   
     temp = U; 
-    for i=1:N
-        sum = 0; 
-        for j=1:N
-            if i~=j
-                sum = sum + A(i,j)*temp(j); 
-            end
-        end
-        U(i) = 1/A(i,i)*(b(i)-sum); 
+    ss = 4*temp; 
+    for index=1:len
+        i = I(index); 
+        j = J(index); 
+        ss(i) = ss(i) + A(i,j)* temp(j); 
     end
-    diff  =  norm(U- true)  
-    iter = iter + 1 ; 
-end 
+    for i=1:N
+        U(i) = 1.0/A(i,i)*(b(i)-ss(i)); 
+    end
+    res  =  norm(b-A*U)/nb      
+    iter = iter + 1; 
+
+end
 
 end
