a34876a18522cb1b98f4fd3347390fdd19b07419
Jun Cheng <chengjun0515@gmail.com>
2015-11-26 23:05:44 -0500

    hw07

---
+++ ME581_numerical_methods/hw07/Runge_Kutta.cpp
@@ -0,0 +1,37 @@
+#include <iostream> 
+
+
+using namespace std; 
+
+double func(double t, double y) {
+	return -16.1*t; 
+}
+
+
+double RK(double y0, double h, int iter_Num) {
+	double a, b, c, d; 
+	double y = y0; 
+	double t = 0; 
+	for(int i=0; i<iter_Num; ++i) {
+		a = func(t, y); 
+		b = func(t+0.5*h, y+0.5*h*a); 
+		c = func(t+0.5*h, y+0.5*h*b); 
+		d = func(t+h, y+h*c); 
+		y = y + h/6*(a+2*b+2*c+d); 
+		t = t + h; 
+	}
+	return y; 
+}
+
+int main() {
+	double h = 0.02; 
+	for(int i=0; i<50; ++i) {
+		double val = RK(0, h, i); 
+		cout << "Iteration: " << i*h << "\t" << val << endl; 
+
+	}
+
+
+
+	return 0; 
+}
