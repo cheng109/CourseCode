6a2b038e9f1f89938e93cc43e7b0a07c4638aa38
Jun Cheng <chengjun0515@gmail.com>
2016-02-17 10:08:27 -0500

    update

---
+++ ece580/funwork02/GoldenSection.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+
+	double upper = 0.55; 
+	double lower = -0.11; 
+	int k=0; 
+
+	double rho = 1/1.618; 
+	double interval = 100; 
+	while (interval >0.1) {
+		
+
+
+
+	}
+
+
+
+
+}
